15251cb564e9aa0b2fe57fcc0dfebe0b558544c0
strike-digital <a@sunadal.co.uk>
2022-03-03 20:53:13 +0000

    BM page first semi working version

--- css_inliner.py
+++ css_inliner.py
@@ -5,8 +5,9 @@ cwd = os.getcwd()
 path = Path(cwd) / "Alpha-trees-BM-page/src/pages/_at_page1.html"
 
 
-with open(str(path), "r") as file:
+with open(str(path), "r", encoding="utf8") as file:
     contents = file.read()
+
 
 # html = """
 # <style>
@@ -18,7 +19,9 @@ with open(str(path), "r") as file:
 inliner = css_inline.CSSInliner(remove_style_tags=True)
 inlined = inliner.inline(contents)
 path = path.parent / "_at_page1_inlined.html"
-with open(str(path), "w") as file:
+with open(str(path), "w", encoding="utf8") as file:
+    for i in ["card", "card-body"]:  # "heading", "card-body", "card-media", "card-text left", "card-text right", "cards"]:
+        inlined = inlined.replace(f"class=\"{i}\"", "")
     file.write(inlined)
 
 print(inlined)
