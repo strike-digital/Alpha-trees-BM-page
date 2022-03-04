e5e3b1f93a6f569bf4dfc68bfce6d7ce9b0dce57
strike-digital <a@sunadal.co.uk>
2022-03-04 12:36:05 +0000

    Final page v1

--- css_inliner.py
+++ css_inliner.py
@@ -1,6 +1,8 @@
 import css_inline
 import os
 from pathlib import Path
+import pyperclip
+
 cwd = os.getcwd()
 path = Path(cwd) / "Alpha-trees-BM-page/src/pages/_at_page1.html"
 
@@ -8,20 +10,13 @@ path = Path(cwd) / "Alpha-trees-BM-page/src/pages/_at_page1.html"
 with open(str(path), "r", encoding="utf8") as file:
     contents = file.read()
 
-
-# html = """
-# <style>
-# h{color:red}
-# </style>
-# <h>HooHoo</h>
-# """
-# contents = html
 inliner = css_inline.CSSInliner(remove_style_tags=True)
 inlined = inliner.inline(contents)
 path = path.parent / "_at_page1_inlined.html"
+for i in ["card", "card-body"]:
+    inlined = inlined.replace(f"class=\"{i}\"", "")
 with open(str(path), "w", encoding="utf8") as file:
-    for i in ["card", "card-body"]:  # "heading", "card-body", "card-media", "card-text left", "card-text right", "cards"]:
-        inlined = inlined.replace(f"class=\"{i}\"", "")
     file.write(inlined)
 
-print(inlined)
+pyperclip.copy(inlined)
+print("Done!")
